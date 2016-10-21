86bc50a3f7c562290feba136c5d671cd3a3dd583
欧阳望 <892988508@qq.com>
2016-10-22 00:24:14 +0800

    first commit

---
+++ Classes/Role/Role.cpp
@@ -0,0 +1,21 @@
+//
+//  Role.cpp
+//  Bcommand
+//
+//  Created by 欧阳望 on 2016/10/21.
+//
+
+#include "Role.hpp"
+
+
+Role::Role(void)
+{
+    
+}
+
+Role::~Role(void)
+{
+    
+}
+
+
---
+++ Classes/Role/Role.hpp
@@ -0,0 +1,33 @@
+//
+//  Role.hpp
+//  Bcommand
+//
+//  Created by 欧阳望 on 2016/10/21.
+//
+//
+//
+// class description
+// All RoleSprite sub this class, the basic class
+
+#ifndef Role_hpp
+#define Role_hpp
+
+#include "Role.hpp"
+#include "cocos2d.h"
+
+using namespace std;
+USING_NS_CC;
+
+class Role : public Node
+{
+public:
+    Role(void);
+    virtual ~Role(void);
+    
+    
+    
+    
+    
+};
+
+#endif /* Role_hpp */
